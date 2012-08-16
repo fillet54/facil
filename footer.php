fa7ae80d5013335f34a36eca1d818ce51e7f074f
Phillip Gomez <gomez.phillip@gmail.com>
2012-08-15 23:11:02 -0700

    Remove pretty photo gallery overlay

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <script type="text/javascript" charset="utf-8">
 $(document).ready(function(){
-   $("a[rel^='prettyPhoto']").prettyPhoto({deeplinking: false});
+   $("a[rel^='prettyPhoto']").prettyPhoto({
+      deeplinking: false,
+      overlay_gallery: false});
 });
 </script>
    </body>
